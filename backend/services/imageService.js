897c2ab7e6d5ab249d0ecb65e4f38c81f3856ac2
Chirag <76880977+chirag127@users.noreply.github.com>
2025-03-10 03:40:08 +0530

    Remove Cloudinary integration and update image upload logic to use Imgur; adjust API URL in frontend environment configuration

--- backend/services/imageService.js
+++ backend/services/imageService.js
@@ -1,29 +1,4 @@
 import axios from "axios";
-import { v2 as cloudinary } from 'cloudinary'
-cloudinary.config({
-    cloud_name: "di0mvijee",
-    api_key: "937372845829153",
-    api_secret:
-        "CLOUDINARY_URL=cloudinary://937372845829153:222sbPe3ZrkjVUbO7asNKfYd8ZI@di0mvijee",
-});
-
-// Function to upload image to Cloudinary
-export const uploadToCloudinary = async (image) => {
-    try {
-        const result = await cloudinary.uploader.upload(image);
-        // You can customize the folder option based on your needs
-
-        // Return the public URL of the uploaded image
-        imageUrl = result.secure_url;
-
-        console.log("Image URL:", imageUrl);
-
-        return imageUrl;
-    } catch (error) {
-        console.error("Error uploading to Cloudinary:", error.message);
-        throw error;
-    }
-};
 
 export const uploadToImgur = async (image) => {
     let imageData = image;
