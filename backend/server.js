d84e049866374eb2de30ffff605d0508361790ff
Chirag <76880977+chirag127@users.noreply.github.com>
2025-03-13 23:29:28 +0530

    Update CORS middleware to support PUT and DELETE methods

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: "*",
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST",`PUT`, `DELETE`],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
